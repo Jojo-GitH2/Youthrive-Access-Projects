14871712315cfe7a9e480eaf0d7f2f4a5b7c1834
Jonah Uka <jonah.uka@gmail.com>
2024-04-25 22:13:46 +0100

    POST, PUT methods Functional

---
+++ Week 3/assignment 1/README.md
@@ -0,0 +1,13 @@
+# Week 3 - Assignment 1
+
+## 1. GET Request to https://jsonplaceholder.typicode.com/posts/1
+
+![alt text](./images/image.png)
+
+## 2. GET Request to https://jsonplaceholder.typicode.com/posts
+
+![alt text](./images/image-1.png)
+
+## 3. POST Request to https://jsonplaceholder.typicode.com/posts
+
+![alt text](./images/image-2.png)
--- Week 3/assignment 1/index.js
+++ Week 3/assignment 1/index.js
@@ -1,23 +1,49 @@
 // Try to fetch data from URLs
-async function fetchFromURLs(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
+async function fetchFromURLs(url, method, body) {
+    try {
+        
+    
+        const response = await fetch(url, {
+            method: method, // Choose the Request Type: GET, POST, PUT, DELETE, OR PATCH
+            body: JSON.stringify(body), // Include the body of the request
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8'
+            }
+        }
+        );
+        console.log("fetched");
+        const data = await response.json();
+        return data;
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 // Get elements from the DOM
 
 const fetchButton = document.getElementById('fetchbtn');
 const url = document.getElementById('url');
+const method = document.getElementById('requestType');
+const body = document.getElementById('body');
 
 // Include a click event listener on the fetch button
 
 fetchButton.addEventListener('click', () => {
     // Get the value of the input field
     const urlValue = url.value;
-
+    const methodValue = method.value;
+    let bodyValue;
+    if (methodValue !== 'GET') {
+        console.log(body, "object");
+        console.log(body.value);
+        bodyValue = JSON.parse(body.value);
+    //     '{
+    //     "id": 1, "title": "foo", "body": "bar", "userId": "1"
+    // } '
+    }
+    
     // Fetch data from the URL
-    fetchFromURLs(urlValue)
+    fetchFromURLs(urlValue, methodValue, bodyValue)
         .then(data => {
             //  Create a new card element
             const card = document.createElement('div');
@@ -25,7 +51,7 @@ fetchButton.addEventListener('click', () => {
 
             // Include the URL requested in the card and type of Request
             const requestInfo = document.createElement('p');
-            requestInfo.innerText = `URL: ${urlValue}`;
+            requestInfo.innerText = `URL: ${urlValue}, Request Type: ${methodValue}`;
             card.appendChild(requestInfo);
 
             //  Include the content of the fetched data in the card
